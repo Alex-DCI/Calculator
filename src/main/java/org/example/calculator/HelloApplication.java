cabe8d080f925cd0bb49ede372985b1b966d0cec
Alex <os.owcharenko@gmail.com>
2026-03-09 20:51:34 +0100

    Stage setup

--- src/main/java/org/example/calculator/HelloApplication.java
+++ src/main/java/org/example/calculator/HelloApplication.java
@@ -1,18 +1,26 @@
 package org.example.calculator;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.rgb(32, 32, 32));
+
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/icons/CalculatorAppList.scale-150.png")).toExternalForm());
+        stage.getIcons().add(icon);
+        stage.setTitle("Calculator");
+        stage.setWidth(340);
+        stage.setHeight(510);
+
         stage.setScene(scene);
         stage.show();
     }
