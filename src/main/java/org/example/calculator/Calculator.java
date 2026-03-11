31caabade8ef1880be236d0e3d779691316bfdaa
Alex <os.owcharenko@gmail.com>
2026-03-11 09:07:16 +0100

    FXML setup

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,26 +1,25 @@
 package org.example.calculator;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class Calculator extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("normal_view.fxml"));
         Group root = new Group();
-        Scene scene = new Scene(root, Color.rgb(32, 32, 32));
-
+        Scene scene = new Scene(loader.load(), Color.rgb(32, 32, 32));
         Image icon = new Image(Objects.requireNonNull(getClass().getResource("/icons/CalculatorAppList.scale-150.png")).toExternalForm());
         stage.getIcons().add(icon);
         stage.setTitle("Calculator");
-        stage.setWidth(340);
-        stage.setHeight(510);
-
         stage.setScene(scene);
         stage.show();
     }
