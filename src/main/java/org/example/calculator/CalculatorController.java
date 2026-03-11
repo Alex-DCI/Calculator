67c6028e9ec1b93bc621b57a9518139b08414632
Alex <os.owcharenko@gmail.com>
2026-03-11 18:38:59 +0100

    Configuring the Controller + FXML

--- src/main/java/org/example/calculator/CalculatorController.java
+++ src/main/java/org/example/calculator/CalculatorController.java
@@ -1,45 +1,137 @@
 package org.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.math.BigDecimal;
 
 public class CalculatorController {
     @FXML
-    private Button plusMinusButton;
+    Label label;
+    BigDecimal total = new BigDecimal(0);
+    String totalString = "0";
+
     @FXML
-    private Button multiplyButton;
+    private void press1(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.ONE);
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button divideButton;
+    private void press2(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.TWO);
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button subtractButton;
+    private void press3(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(3L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button sumButton;
+    private void press4(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(4L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button equalsButton;
+    private void press5(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(5L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button clearButton;
+    private void press6(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(6L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button clearAllButton;
+    private void press7(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(7L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button button0;
+    private void press8(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(8L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button button1;
+    private void press9(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(9L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button button2;
+    private void press0(ActionEvent event) {
+        total = total.multiply(BigDecimal.TEN);
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button button3;
+    private void pressPlusMinus(ActionEvent event) {
+        total = total.multiply(BigDecimal.valueOf(-1L));
+        totalString = total.toString();
+        label.setText(totalString);
+    }
     @FXML
-    private Button button4;
+    private void pressComma(ActionEvent event) {
+        if(!totalString.contains(",")) {
+            totalString += ',';
+            label.setText(totalString);
+            total = total.multiply(BigDecimal.valueOf(1.0));
+        }
+    }
     @FXML
-    private Button button5;
+    private void pressEquals(ActionEvent event) {
+
+    }
     @FXML
-    private Button button6;
+    private void pressMultiply(ActionEvent event) {
+
+    }
     @FXML
-    private Button button7;
+    private void pressDivide(ActionEvent event) {
+
+    }
     @FXML
-    private Button button8;
+    private void pressSum(ActionEvent event) {
+
+    }
     @FXML
-    private Button button9;
+    private void pressSubtract(ActionEvent event) {
+
+    }
     @FXML
-    private Button sumButton;
+    private void pressDivideOneByX(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressSquare(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressSquareRoot(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressPercent(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressClear(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressClearAll(ActionEvent event) {
+
+    }
+    @FXML
+    private void pressBackspace(ActionEvent event) {
+
+    }
 }
